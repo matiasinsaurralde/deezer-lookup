938695a94803c3109128f5d32ad587a84e60e54b
Matias Insaurralde <matias@insaurral.de>
2014-09-19 03:10:42 -0400

    dealing with thresholds

--- lib/deezer-lookup.rb
+++ lib/deezer-lookup.rb
@@ -18,7 +18,21 @@ module Deezer
     base_url = "http://api.deezer.com/search?access_token=#{@access_token}&q="
     base_url += URI.escape( query.join(' ').downcase )
     raw_json = open(base_url).read()
-    data = JSON.parse(raw_json)['data']
+
+    results, input_song_title, input_artist_name, filtered_results = JSON.parse(raw_json)['data'], query[0].downcase, query[1].downcase, []
+
+    results.each do |r|
+      song_title, artist_name = r['title'].downcase, r['artist']['name'].downcase
+      r['levenshtein'] = [ DamerauLevenshtein.distance(song_title, input_song_title), DamerauLevenshtein.distance(artist_name, input_artist_name) ]
+    end
+
+    results.each do |r|
+      song_title, artist_name = r['title'].downcase, r['artist']['name'].downcase
+      if r['levenshtein'][0] <= @levenshtein_thresholds[:song] and r['levenshtein'][1] <= @levenshtein_thresholds[:artist]
+        filtered_results.push(r)
+      end
+    end
+
   end
 
 end
