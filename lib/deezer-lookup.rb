81f3bace083301388ba77ef6fe23575311af95fe
Matias Insaurralde <matias@insaurral.de>
2014-09-19 02:17:47 -0400

    variation stuff

--- lib/deezer-lookup.rb
+++ lib/deezer-lookup.rb
@@ -2,16 +2,23 @@ require 'damerau-levenshtein'
 require 'open-uri'
 require 'json'
 
-LEVENSHTEIN_THRESHOLDS = []
+LEVENSHTEIN_THRESHOLDS = { :song => 0, :artist => 0 } # by default don't accept variations
 
 module Deezer
-  def self.access_token(token)
-    @access_token = token
+
+  def self.access_token(token, levenshtein_thresholds = LEVENSHTEIN_THRESHOLDS)
+    @access_token, @levenshtein_thresholds = token, levenshtein_thresholds
   end
+
+  def self.levenshtein_thresholds=(thresholds)
+    @levenshtein_thresholds = thresholds
+  end
+
   def self.lookup(*query)
     base_url = "http://api.deezer.com/search?access_token=#{@access_token}&q="
     base_url += URI.escape( query.join(' ').downcase )
     raw_json = open(base_url).read()
     data = JSON.parse(raw_json)['data']
   end
+
 end
